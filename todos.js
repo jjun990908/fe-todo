c4591723d1754ac7d230571761ac86d4893a80c4
jjun990908 <jjun990908@naver.com>
2023-07-03 18:23:32 +0900

    [init] todos 추가

---
+++ todos.js
@@ -0,0 +1,18 @@
+//todos.js
+
+let todos = [
+  {
+    name: "자바스크립트 공부하기",
+    tags: ["programming", "javascript"],
+    status: "todo",
+    id: 12123123,
+  },
+  {
+    name: " 그림 그리기",
+    tags: ["picture", "favorite"],
+    status: "doing",
+    id: 312323,
+  },
+];
+
+export default todos;
