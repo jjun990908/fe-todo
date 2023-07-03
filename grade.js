a931695a3c23e435f844ab6d1ae430eb7fb13533
jjun990908 <jjun990908@naver.com>
2023-07-03 18:24:03 +0900

    [feat] show all 함수 기능 구현

---
+++ grade.js
@@ -0,0 +1,62 @@
+var readline = require("readline");
+// const { todos } = require("./todos");
+
+let todos = [
+  {
+    name: "자바스크립트 공부하기",
+    tags: ["programming", "javascript"],
+    status: "todo",
+    id: 12123123,
+  },
+  {
+    name: " 그림 그리기",
+    tags: ["picture", "favorite"],
+    status: "doing",
+    id: 312323,
+  },
+];
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var recursiveAsyncReadLine = function () {
+  rl.question("명령어를 입력하세요: ", function (answer) {
+    if (answer == "exit") return rl.close();
+
+    input = answer.split("$");
+    if (input[0] === "show") {
+      show(input[1]);
+    }
+
+    recursiveAsyncReadLine();
+  });
+};
+
+recursiveAsyncReadLine();
+
+const show = function (status) {
+  //show 함수
+  if (status === "all") {
+    // input 값이 all 일때
+    todo = 0;
+    doing = 0;
+    done = 0;
+    todos.forEach((element) => {
+      status = element.status;
+      if (status === "todo") {
+        todo += 1;
+      } else if (status === "doing") {
+        doing += 1;
+      } else if (status === "done") {
+        done += 1;
+      }
+    });
+    console.log(`todo: ${todo}개, doing: ${doing}개, done: ${done}개`);
+  } else if (status === "todo") {
+  } else if (status === "doing") {
+  } else if (status === "done") {
+  } else {
+  }
+};
