478bbb14cb74337434b92d1ace36e9fdcde7e907
jjun990908 <jjun990908@naver.com>
2023-07-04 17:35:09 +0900

    [fix] code 리팩토링

--- grade.js
+++ grade.js
@@ -7,24 +7,6 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function print_Help() {
-  console.log("\n\t\t\t사용 가능한 명령어 목록");
-  console.log(
-    "===================================================================="
-  );
-  console.log(
-    " show\t: [ all || status(todo, doing, done) || name || tags || id ]"
-  );
-  console.log(" add\t: [ name $ id ]");
-  console.log(" delete : [ id ]");
-  console.log(" update : [ id $ status ]");
-  console.log(" help");
-  console.log(" exit");
-  console.log(
-    "====================================================================\n\n"
-  );
-}
-
 //  입력 반복 받기
 const recursiveAsyncReadLine = function () {
   rl.question("명령어를 입력하세요: ", function (answer) {
@@ -33,20 +15,20 @@ const recursiveAsyncReadLine = function () {
     const command = input[0];
     switch (command) {
       case "show":
-        show(input[1]);
+        showItem(input[1]);
         break;
       case "add":
         const tag = JSON.parse(input[2]);
-        add(input[1], tag);
+        addItem(input[1], tag);
         break;
       case "delete":
-        delete_item(input[1]);
+        deleteItem(input[1]);
         break;
       case "update":
-        update(input[1], input[2]);
+        updateItem(input[1], input[2]);
         break;
       case "help":
-        print_Help();
+        printHelp();
         break;
       case "exit":
         return rl.close();
@@ -60,7 +42,7 @@ const recursiveAsyncReadLine = function () {
 recursiveAsyncReadLine();
 
 // show 명령어 함수
-const show = function (status) {
+const showItem = function (status) {
   const count = {
     todo: 0,
     doing: 0,
@@ -78,54 +60,44 @@ const show = function (status) {
     );
   } else if (status === "tags" || status === "name" || status === "id") {
     let line = `총 ${count.all}건의 ${status} :`;
-    todos.map((item) => (line += `'${item[status]}',`));
+    todos.forEach((item) => (line += `'${item[status]}',`));
     line = line.slice(0, -1);
     console.log(line);
   } else {
-    const result = todos.filter((todo) => todo.status === status);
+    const selectedTodo = todos.filter((todo) => todo.status === status);
     let line = `${status} 리스트 : 총 ${count[status]}건 : `;
-    result.map((item) => (line += `'${item.name}, ${item.id}',`));
+    selectedTodo.forEach((item) => (line += `'${item.name}, ${item.id}',`));
     line = line.slice(0, -1);
     console.log(line);
   }
 };
 
 // add 명령어 함수
-const add = function (name, tag) {
-  let random = Math.floor(Math.random() * 1000000) + 1;
-  const check_name = todos.filter((item) => item.name === name);
-  if (check_name.length >= 1) {
-    console.log("입력하신 이름과 같은 TODO리스트가 존재합니다.");
+const addItem = function (name, tag) {
+  let newId = getId();
+  if (!checkValidation(name)) {
     return;
-  }
-  while (true) {
-    let check_id = todos.filter((item) => item.id === random);
-    if (check_id.length >= 1) {
-      random = Math.floor(Math.random() * 1000000) + 1;
-    } else {
-      break;
-    }
   }
   const newItem = {
     name: name,
     tag: tag,
     status: "todo",
-    id: random,
+    id: newId,
   };
   todos.push(newItem);
-  console.log(`${name}이 1개가 추가됐습니다.(id:${random})`);
-  show("all");
+  console.log(`${name}이 1개가 추가됐습니다.(id:${newId})`);
+  showItem("all");
 };
 
 // delete 명령어 함수
-const delete_item = function (id) {
+const deleteItem = function (id) {
   let flag = false;
   todos.forEach((item, idx) => {
     if (item.id === parseInt(id)) {
       todos.splice(idx, 1);
       console.log(`${item.name} ${item.status}가 목록에서 삭제되었습니다.`);
       flag = true;
-      show("all");
+      showItem("all");
     }
   });
   if (!flag) {
@@ -134,17 +106,66 @@ const delete_item = function (id) {
 };
 
 // update 명령어 함수
-const update = function (id, status) {
+const updateItem = function (id, status) {
   let flag = false;
-  todos.forEach((item, idx) => {
+  todos.forEach((item) => {
     if (item.id === parseInt(id)) {
       item.status = status;
       console.log(`${item.name} ${item.status}으로 상태가 변경됐습니다`);
       flag = true;
-      show("all");
+      showItem("all");
     }
   });
   if (!flag) {
     console.log("입력하신 id값에 해당하는 값이 존재하지 않습니다.");
   }
 };
+
+const getId = function () {
+  const getRandomId = () => Math.floor(Math.random() * 10000000) + 1;
+  let newId = getRandomId();
+  while (true) {
+    let idValidation = true;
+    todos.forEach((item) => {
+      if (item.id === newId) {
+        newId = getRandomId();
+        idValidation = false;
+      }
+    });
+    if (idValidation) {
+      return newId;
+    }
+  }
+};
+
+const checkValidation = function (name) {
+  let nameValidation = false;
+  todos.forEach((item) => {
+    if (item.name === name) {
+      nameValidation = true;
+    }
+  });
+  if (nameValidation) {
+    console.log("이미 존재하는 이름입니다.");
+    return false;
+  }
+  return true;
+};
+
+function printHelp() {
+  console.log("\n\t\t\t사용 가능한 명령어 목록");
+  console.log(
+    "===================================================================="
+  );
+  console.log(
+    " show\t: [ all || status(todo, doing, done) || name || tags || id ]"
+  );
+  console.log(" add\t: [ name $ id ]");
+  console.log(" delete : [ id ]");
+  console.log(" update : [ id $ status ]");
+  console.log(" help");
+  console.log(" exit");
+  console.log(
+    "====================================================================\n\n"
+  );
+}
